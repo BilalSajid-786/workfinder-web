5ccd2b1672bebfa4292968cba862022a27c92f64
talraza <talha.raza.1904@gmail.com>
2025-10-10 00:08:31 +0500

    Active Jobs Table

--- src/app/components/active-jobs/active-jobs.component.ts
+++ src/app/components/active-jobs/active-jobs.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { JobService } from '../../services/job.service';
 import { ToastrService } from 'ngx-toastr';
 import { Job } from '../../models/job.model';
+import { PagingService } from '../../services/paging.service';
 
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -26,11 +28,12 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    MatSlideToggleModule
   ],
   templateUrl: './active-jobs.component.html',
   styleUrl: './active-jobs.component.scss',
 })
-export class ActiveJobsComponent implements OnInit {
+export class ActiveJobsComponent implements AfterViewInit {
   displayedColumns: string[] = [
     'title',
     'industryName',
@@ -40,37 +43,66 @@ export class ActiveJobsComponent implements OnInit {
     'actions',
   ];
 
+  dataTable = {
+    searchValue: '',
+    sortColumn: 'title',
+    sortOrder: 'asc',
+    length: 100,
+    pageSize: 5,
+    pageNo: 0,
+    pageIndex: 0
+  };
+
+
   dataSource = new MatTableDataSource<Job>([]);
   activeJobs: Job[] = [];
+  totalCount: number = 0;
+  EnterSearchValue:string='';
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  
 
-  constructor(private jobService: JobService, private toastr: ToastrService) {}
+  constructor(private jobService: JobService, private toastr: ToastrService, private pagingService: PagingService) {}
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
+  //   this.getActiveJobs();
+  // }
+
+  ngAfterViewInit(): void {
+    this.getActiveJobs();
+    //this.dataSource.paginator = this.paginator;
+  }
+
+  OnSearchTextChange(){
+    //console.log(this.EnterSearchValue);
+    this.dataTable.searchValue=this.EnterSearchValue;
     this.getActiveJobs();
   }
 
-  ngAfterViewInit(): void {
-    // attach once view init; data will connect after API returns
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-
-    // custom filter by title, city, industry
-    this.dataSource.filterPredicate = (data: Job, filter: string) => {
-      const str = (
-        data.title +
-        ' ' +
-        data.city +
-        ' ' +
-        data.industryName
-      ).toLowerCase();
-      return str.includes(filter.trim().toLowerCase());
-    };
+  getPagingSizeIntervals(): number[] {
+    return this.pagingService.getPagingSizeIntervals();
   }
 
+  tableSortChange(event: any) {
+    console.log("Sort Event", event);
+    this.dataTable.sortColumn = event.active;
+    this.dataTable.sortOrder = event.direction;
+    this.getActiveJobs();
+  }
+
+  handlePage(event: PageEvent) {
+    //console.log("handle page", event);
+    this.dataTable.pageIndex = event.pageIndex;
+    this.dataTable.pageSize = event.pageSize;
+    this.getActiveJobs();
+  }
+
+
+
   applyFilter(value: string): void {
+    console.log("applyfilter", value);
     this.dataSource.filter = value;
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
@@ -78,22 +110,23 @@ export class ActiveJobsComponent implements OnInit {
   }
 
   getActiveJobs(): void {
-    const payload: any = {
-      searchValue: '',
-      sortColumn: 'JobTitle',
-      sortOrder: 'Asc',
-      pageSize: 10,
-      pageNo: 1,
-      employerId: '7FBD2975-5FE2-476F-B4A8-9B2DC2A331EB',
-    };
-    this.jobService.GetActiveJobs(payload).subscribe({
-      next: (res: any) => {
+    this.dataTable.pageNo = this.dataTable.pageIndex + 1;
+
+    this.jobService.GetActiveJobs(this.dataTable).subscribe({
+      next: (res) => {
+        //console.log('res', res);
         this.activeJobs = res.result;
-        console.log('activeJobs', this.activeJobs);
+        this.totalCount = res.result[0].totalRows;
+        this.dataTable.length = this.totalCount;
+        //console.log('activeJobs', this.activeJobs);
         this.dataSource.data = this.activeJobs;
-        console.log('dataSource', this.dataSource.data);
+        //console.log('dataSource', this.dataSource.data);
       },
       error: () => this.toastr.error('Failed to get jobs'),
     });
+  }
+
+  onToggleStatus(job: Job, checked: boolean) {
+
   }
 }
---
+++ src/app/services/paging.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagingService {
+
+  getPagingSizeIntervals(): number[] {
+    return [5, 10, 20, 50];
+  }
+}
